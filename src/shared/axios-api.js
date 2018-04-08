974fbe31999dbfb4236ebc09dd8ee16a1058fb64
jamesonbass1987 <jamesonbass@gmail.com>
2018-04-07 19:01:40 -0700

    updated axios base url for api call

--- src/shared/axios-api.js
+++ src/shared/axios-api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://meet-fido-pdx-api.herokuapp.com',
-    headers: { Authorization: localStorage.getItem('token') }
+  baseURL: "http://meet-fido-pdx-api.herokuapp.com/api/v1",
+  headers: { Authorization: localStorage.getItem("token") }
 });
 
 export default instance;
