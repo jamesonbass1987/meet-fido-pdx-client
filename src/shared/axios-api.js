7a909417d5cef0930d716884b2a4dffb1647651c
jamesonbass1987 <jamesonbass@gmail.com>
2018-04-07 19:13:50 -0700

    added https

--- src/shared/axios-api.js
+++ src/shared/axios-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: "http://meet-fido-pdx-api.herokuapp.com/api/v1",
+  baseURL: "https://meet-fido-pdx-api.herokuapp.com/api/v1",
   headers: { Authorization: localStorage.getItem("token") }
 });
 
