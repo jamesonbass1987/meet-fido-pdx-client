c782d5185e9e26fb8d533138986cd761ec3fd4ad
jamesonbass1987 <jamesonbass@gmail.com>
2018-04-07 19:34:15 -0700

    updated axios headers

--- src/shared/axios-api.js
+++ src/shared/axios-api.js
@@ -3,12 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: "https://meet-fido-pdx-api.herokuapp.com/api/v1",
   headers: {
-    Authorization: localStorage.getItem("token"),
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Credentails": true,
-    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,OPTIONS",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+    Authorization: localStorage.getItem("token")
   }
 });
 
