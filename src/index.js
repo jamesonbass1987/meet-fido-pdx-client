f75c1dfe163abb3c146a342fcb3c77b566a2cfe6
jamesonbass1987 <jamesonbass@gmail.com>
2018-04-15 19:16:53 -0700

    removed redux dev tools

--- src/index.js
+++ src/index.js
@@ -13,10 +13,9 @@ import registerServiceWorker from './registerServiceWorker';
 
 require('dotenv').config()
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers = compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)
-));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
     <Provider store={store}>
